d845271fbbb6997d418411655055e00b8e71cbf0
Prakash Meena <pcmeena511@gmail.com>
2026-04-24 21:33:02 +0530

    Implement negative cycle detection in Floyd-Warshall
    Added negative cycle detection to Floyd-Warshall algorithm.

--- Graph Algorithms/FloydWarshall.cpp
+++ Graph Algorithms/FloydWarshall.cpp
@@ -4,29 +4,34 @@
 // Maintain p[][] for getting path.Recursively find path between i and p[i][j], p[i][j] and j.
 
 void FloydWarshall(int n, vector<pair<int, ll>> *adj, vector<vector<ll>> &dist) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			dist[i][j] = INF;
-		}
-		dist[i][i] = 0;
-	}
-	for (int i = 0; i < n; i++) {
-		for (auto j : adj[i]) {
-			dist[i][j.ff] = min(dist[i][j.ff], j.ss);
-		}
-	}
-	for (int k = 0; k < n; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (dist[i][k] < INF && dist[k][j] < INF) // for updating only when there is a path
-					dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-			}
-		}
-	}
-}
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist[i][j] = INF;
+        }
+        dist[i][i] = 0;
+    }
 
-for (int i = 0; i < n; i++) {
-    if (dist[i][i] < 0) {
-        cout << "Negative Cycle detected\n";
+    for (int i = 0; i < n; i++) {
+        for (auto j : adj[i]) {
+            dist[i][j.first] = min(dist[i][j.first], j.second);
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    // Negative cycle detection
+    for (int i = 0; i < n; i++) {
+        if (dist[i][i] < 0) {
+            cout << "Negative Cycle detected\n";
+        }
     }
 }
