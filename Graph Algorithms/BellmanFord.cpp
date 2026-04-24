5aef9443389ee1014274df7cda10c3a2417d7532
Prakash Meena <pcmeena511@gmail.com>
2026-04-24 21:02:24 +0530

    Update BellmanFord.cpp

--- Graph Algorithms/BellmanFord.cpp
+++ Graph Algorithms/BellmanFord.cpp
@@ -1,26 +1,39 @@
 //O(E) + O(nE) + O(nE) ≈ O(nE)
 
 void BellmanFord(int n, int src, vector<pair<int, ll>> *edges, vector<ll>& dist, set<int>& negCycle) {
-	fill(all(dist), INF);
-	dist[src] = 0;
-	vector<pair<ll, pair<int, int>>> e;
-	for (int i = 0; i < n; i++) {
-		for (auto j : edges[i]) {
-			e.pb({j.ss, {i, j.ff}});
-		}
-	}
-	for (int i = 0; i < n - 1; i++) {
-		for (auto j : e) {
-			dist[j.ss.ss] = min(dist[j.ss.ss], dist[j.ss.ff] + j.ff);
-		}
-	}
-	//checking negative cycle
-	for (int i = 0; i < n; i++) {
-		for (auto j : e) {
-			if (dist[j.ss.ss] > dist[j.ss.ff] + j.ff) {
-				dist[j.ss.ss] = dist[j.ss.ff] + j.ff;
-				negCycle.insert(j.ss.ss);
-			}
-		}
-	}
+    
+    fill(all(dist), INF);
+    dist[src] = 0;
+
+    vector<pair<ll, pair<int, int>>> e;
+
+    // build edge list
+    for (int i = 0; i < n; i++) {
+        for (auto j : edges[i]) {
+            e.push_back({j.second, {i, j.first}});
+        }
+    }
+
+    // relax edges
+    for (int i = 0; i < n - 1; i++) {
+        for (auto j : e) {
+            if (dist[j.second.first] != INF &&
+                dist[j.second.first] + j.first < dist[j.second.second]) {
+                
+                dist[j.second.second] = dist[j.second.first] + j.first;
+            }
+        }
+    }
+
+    // negative cycle
+    for (int i = 0; i < n; i++) {
+        for (auto j : e) {
+            if (dist[j.second.first] != INF &&
+                dist[j.second.first] + j.first < dist[j.second.second]) {
+                
+                dist[j.second.second] = dist[j.second.first] + j.first;
+                negCycle.insert(j.second.second);
+            }
+        }
+    }
 }
