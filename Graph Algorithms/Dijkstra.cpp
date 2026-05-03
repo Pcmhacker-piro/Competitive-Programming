5f1061ffcbb4cabb8b3decaf4bd85eaffdd09044
Prakash Meena <pcmeena511@gmail.com>
2026-05-03 22:44:37 +0530

    Update Dijkstra.cpp

--- Graph Algorithms/Dijkstra.cpp
+++ Graph Algorithms/Dijkstra.cpp
@@ -1,25 +1,34 @@
-// O(VlogV + ElogV)
-// Single Source Shortest Path
+/*
+    Dijkstra's Algorithm (Shortest Path)
+    Time: O((V + E) log V)
+*/
+#include <bits/stdc++.h>
+using namespace std;
 
-void Dijkstra(int s, int n, vector<ll> &dist, vector<int> &parent, vector<pair<int, ll>> *adj) {
-	dist.assign(n, INF);
-	parent.assign(n, -1);
-	dist[s] = 0;
-	priority_queue <pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q;
-	q.push({0, s});
-	while (!q.empty()) {
-		pair<ll, int> here = q.top();
-		q.pop();
-		int v = here.ss;
-		ll d_v = here.ff;
-		if (d_v != dist[v])
-			continue;
-		for (auto edge : adj[v]) {
-			if (dist[v] + edge.ss < dist[edge.ff]) {
-				dist[edge.ff] = dist[v] + edge.ss;
-				parent[edge.ff] = v;
-				q.push({dist[edge.ff], edge.ff});
-			}
-		}
-	}
+#define pii pair<int,int>
+#define vi vector<int>
+
+vector<int> dijkstra(int n, vector<pii> adj[], int src) {
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    vi dist(n + 1, 1e9);
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); 
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (auto &it : adj[u]) {
+            int v = it.first, w = it.second;
+
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    return dist;
 }
